a92b09ef1e4392bdac4722c9d579847b51afb164
Aditya kumar <158504078+adityak12322@users.noreply.github.com>
2026-04-30 21:27:31 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -7,7 +7,7 @@ const Login = ({ onSuccess }) => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const { data } = await axios.post('http://localhost:5000/api/auth/login', formData);
+            const { data } = await axios.post('https://task-manager-production-33e6.up.railway.app/api/auth/login', formData);
             localStorage.setItem('token', data.token);
             localStorage.setItem('name', data.name);
             localStorage.setItem('role', data.role);
