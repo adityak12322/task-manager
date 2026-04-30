2b92a1b0f4be17aca4ec2925be38f12faebe8524
Aditya kumar <158504078+adityak12322@users.noreply.github.com>
2026-04-30 21:07:57 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ connectDB();
 
 app.use(express.json());
 app.use(cors({
-    origin: ["https://task-manager-lime-mu-34.vercel.app/", "http://localhost:3000"], // Apne Vercel link ko yahan dalo
+    origin: ["https://task-manager-lime-mu-34.vercel.app", "http://localhost:3000"], // Apne Vercel link ko yahan dalo
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
